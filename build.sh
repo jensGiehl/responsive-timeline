4829b905b1ed8a70cd1cfa9296347b2fec875953
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-17 22:19:06 +0100

    Generate index.html
    From index.html.template and context.txt

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 cmd="docker run --rm -it -v "$(pwd):/opt/work" responsive-timeline"
 
 $cmd sh -c "npm install; gulp;"
+$cmd sh -c "bash-templater/templater.sh index.html.template -f content.txt > index.html"
