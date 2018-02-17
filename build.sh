1cda6761a28acb83d609919357f46ccec55bbba9
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-17 22:37:51 +0100

    Testing with user input (read)

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@
 cmd="docker run --rm -it -v "$(pwd):/opt/work" responsive-timeline"
 
 $cmd sh -c "npm install; gulp;"
-$cmd sh -c "bash-templater/templater.sh index.html.template -f content.txt > index.html"
+$cmd sh -c "echo \"Please enter your first name:\" && read firstName"
+$cmd sh -c "echo \"Hello $firstName. What's your family name?:\" && read lastName"
+$cmd sh -c "echo $firstName"
+$cmd sh -c "echo 'Thanks. And what's the title of the timeline?:' && read title"
+$cmd sh -c "echo 'Fine!'"
+$cmd sh -c "while read p; do \ echo $p \ done <content.txt
+#$cmd sh -c "bash-templater/templater.sh index.html.template -f content.txt > index.html"
